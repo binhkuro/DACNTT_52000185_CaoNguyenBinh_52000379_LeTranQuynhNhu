3362d7312ddf259960450e74266209469b44c037
Bình Nguyên <52000185@student.tdtu.edu.vn>
2023-12-31 00:56:08 +0700

    Xây dựng ứng dụng bước đầu tiên

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# DACNTT2_52000185_CaoNguyenBinh_52000379_LeTranQuynhNhu
+# DỰ ÁN QUẢN LÝ MÔI TRƯỜNG VÀ SỨC KHỎE THÚ CƯNG
+
+## Thông tin nhóm
+
+- 52000185 - Cao Nguyên Bình
+- 52000379 - Lê Trần Quỳnh Như
+
+## Hướng dẫn chạy website
+
+1. Clone project trên Github về máy tính.
+2. Mở cmd và gõ lệnh `yarn install` để tải các module trong file `package.json`.
+3. Mở cmd và gõ lệnh `node app.js`.
+
+Mở trình duyệt và truy cập vào địa chỉ `http://localhost:8080`.
+
+## Một số tài khoản mẫu dùng để test chương trình
+
+### Tài khoản admin
+
+- Tài khoản: admin
+- Mật khẩu: admin
+
+### Tài khoản nhân viên
+
+- Tài khoản: quynhnhu1609
+- Mật khẩu: quynhnhu1609
+
+## Video demo
+
+[![Video Demo](http://img.youtube.com/vi/bJZ9QppnX20/0.jpg)](https://youtu.be/bJZ9QppnX20 "Video Demo")
---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const mongoose = require("mongoose"); // giúp tương tác database
+require("dotenv").config(); // giúp tương tác file .env
+const cookieParser = require('cookie-parser'); // cookie
+const hbs = require('express-handlebars')
+const session = require('express-session'); // session
+const flash = require('connect-flash'); // flash message
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+// Import các module controller
+
+
+// Lấy dữ liệu từ file .env ra
+const PORT = process.env.PORT;
+const CONNECTION_STRING = process.env.CONNECTION_STRING;
+const COOKIE_SIGN = process.env.COOKIE_SIGN;
+
+// Cấu hình các package đã import
+let app = express()
+app.use(express.static(__dirname + '/public')) // truy cập thư mục public để sử dụng img, css, js,...
+app.use(express.json()); // lấy dữ liệu từ request dưới dạng JSON
+app.use(express.urlencoded({ extended: true })); // lấy dữ liệu từ request dưới dạng url-encoded
+app.use(cookieParser(COOKIE_SIGN)); // cookie
+app.use(session({ // session
+    secret: COOKIE_SIGN,
+    resave: true,
+    saveUninitialized: true,
+}));
+app.use(flash()) // flash message
+app.engine('handlebars', hbs.engine({
+    defaultLayout: 'main',
+    helpers: {
+        eq: (value1, value2, options) => {
+            return value1 === value2;
+        },
+    }
+}))
+app.set('view engine', 'handlebars')
+
+app.get("/", (req, res) => {
+    res.render('index', {
+        title: 'Trang Chủ',
+    });
+});
+
+// Middle ware 404 error
+app.use((req, res) => {
+    res.status(404)
+    res.render('404', { layout: null })
+})
+
+// Middle ware 500 error
+app.use((err, req, res, next) => {
+    console.error(err.message)
+    res.status(500)
+    res.render('500', { layout: null })
+})
+
+// Kết nối tới database ()
+// mongoose.connect(CONNECTION_STRING)
+//     .then(() => {
+//         console.log('Database connected');
+//         app.listen(PORT); // Tạo server trên cổng 8080 hoặc PORT từ .env
+//     })
+//     .catch((error) => {
+//         console.log('Error connecting to database', error);
+//     });
+
+console.log('Database connected');
+app.listen(PORT);
