efcbb051da6182ffd293266f2672fedde242e0c7
quynhnhu1692 <letranquynhnhu1692@gmail.com>
2024-02-25 17:09:05 +0700

    Chỉnh sửa giao diện sức khỏe, thú cưng, sức khỏe thú cưng

--- public/js/environment.js
+++ public/js/environment.js
@@ -20,31 +20,3 @@ function updateDeviceStatusImage(elementId, status) {
     var imageSrc = status === 'on' ? '/img/on.png' : '/img/off.png';
     document.getElementById(elementId).src = imageSrc;
 }
-
-function updateTemperatureColor(temperature) {
-    var tempValue = parseFloat(temperature);
-
-    if (tempValue < 22) {
-        document.querySelector('.temperature').style.backgroundColor = '#0000FF';
-    } else if (tempValue >= 22 && tempValue <= 24) {
-        document.querySelector('.temperature').style.backgroundColor = '#01FF01';
-    } else if (tempValue > 24) {
-        document.querySelector('.temperature').style.backgroundColor = '#FF0000';
-    }
-}
-
-updateTemperatureColor(document.getElementById('temperature').innerText);
-
-function updateHumidityColor(humidity) {
-    var tmpValue = parseFloat(humidity);
-
-    if (tmpValue < 40) {
-        document.querySelector('.humidity').style.backgroundColor = '#FF0000';
-    } else if (tmpValue >= 40 && tmpValue <= 60) {
-        document.querySelector('.humidity').style.backgroundColor = '#01FF01';
-    } else if (tmpValue > 60) {
-        document.querySelector('.humidity').style.backgroundColor = '#FF0000';
-    }
-}
-
-updateHumidityColor(document.getElementById('humidity').innerText);
