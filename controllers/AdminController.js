49cca76a74624a963fbfc306d48f744201b38085
quynhnhu1692 <letranquynhnhu1692@gmail.com>
2024-02-27 16:03:57 +0700

    Chỉnh giao diện home admin

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,14 +1,11 @@
 let Account = require("../models/account");
 
 async function getAdminHomePage(req, res) {
-    let accounts = await Account.find({ username: { $ne: "admin" } }).lean().exec();
-
     res.render('home-admin', {
         title: 'Trang chủ quản lý',
         username: req.session.username,
         fullname: req.session.fullname,
         profilePicture: req.session.profilePicture,
-        accounts: accounts,
         layout: 'admin',
     });
 }
